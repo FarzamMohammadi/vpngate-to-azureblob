c20ef00d3efeec2d18bb64ed9a783dca5c57b8d1
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2023-03-12 11:24:46 -0400

    Get ovpn files & append additional required configs to it

--- sweeper-duweeper/Scraper/scraper.py
+++ sweeper-duweeper/Scraper/scraper.py
@@ -1,27 +1,83 @@
+import base64
+import fileinput
+import re
 import requests
 from bs4 import BeautifulSoup
-import csv
-import os.path
-from itertools import islice
-import pathlib
+
 
 class Scraper:
+
     def __init__(self, url):
         self.url = url
         self.page = requests.get(self.url)
-        soup = BeautifulSoup(self.page.content, "html.parser")
-        reader = csv.reader(soup.get_text(separator='\n\n'))
-        # print(soup.get_text(separator='\n'))
-        for row in reader:
-            print(row)
-            # openvpn_base64file = row[14]
-            # filepath = pathlib.Path(__file__).parent.resolve() + '/' + index
-            # print(filepath)
-            # with open(filepath, "wb") as fh:
-            #     fh.write(openvpn_base64file.decode('base64'))
+        html_text = BeautifulSoup(self.page.content, 'html.parser').get_text()
 
-            # print(data)     
-        # table = soup.find('table', id="vg_hosts_table_id")
-        # print(table)
+        separated_text = re.split(',,|\n', html_text)
 
-#15
+        for row in separated_text:
+            file_in_string_format = self.base64_to_string(row.strip())
+
+            if file_in_string_format is not None:
+                # The client needs some additional configs in the .ovpn file to work
+                file_in_string_format = self.append_additional_required_vpn_configurations(file_in_string_format)
+
+                return
+
+    def append_additional_required_vpn_configurations(self, file_in_string_format):
+        heading_of_place_to_update = '# It is not recommended to modify it unless you have a particular need.'
+        new_file_array = []   
+    
+        layer_of_connection = None
+        cipher_fallback = None
+        new_line = None
+
+        layer_key = 'dev'
+        cipher_key = 'cipher'
+
+        for line in file_in_string_format.split('\n'):
+
+            if (layer_of_connection is None 
+                and self.is_layer_of_vpn_connection(line, layer_key)):
+                layer_of_connection = line
+
+            if (cipher_fallback is None 
+                and self.is_cipher(line, cipher_key)):
+                cipher_fallback = self.convert_cipher_to_fallback(line, cipher_key)
+
+            if (layer_of_connection
+                and cipher_fallback
+                and new_line is None # Prevents going through this once we attain the new line
+                and line.strip() == heading_of_place_to_update):
+                additional_required_configurations = f'\n\n{layer_of_connection}\n{cipher_fallback}'
+                
+                new_line = line + additional_required_configurations
+
+                new_file_array.append(new_line)
+                continue
+
+            new_file_array.append(line)
+
+        new_file = '\n'.join(new_file_array)
+
+        return new_file
+
+        
+
+    def base64_to_string(self, s):
+        try:
+            return base64.b64decode(s).decode('utf-8')
+        except:
+            return None
+        
+    def is_layer_of_vpn_connection(self, s_line, layer_key):
+        return s_line[:3] == layer_key
+    
+    def is_cipher(self, s_line, cipher_key):
+         return s_line[:6] == cipher_key
+    
+    def convert_cipher_to_fallback(self, s_line, cipher_key):
+        return s_line.replace(cipher_key, 'data-ciphers-fallback')
+        
+            
+            
+
